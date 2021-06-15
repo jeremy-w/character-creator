bf13859540ee79c1b4da438e81f45cbbf1d19cb3
Jeremy W. Sherman <jeremy@bignerdranch.com>
2021-06-15 17:28:55 -0400

    Add perspective bar with layering issue
    - Good: behind footer and character
    - Bad: over the control panel!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
+      <div className={styles.perspective} />
       <CharacterEditor />
       <Footer />
     </>
